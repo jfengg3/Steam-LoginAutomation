2abbb3a9c1efadcf48e3f8c9d309fd500127f0d1
chadjesoo <evelyst21@gmail.com>
2017-06-20 22:33:52 +0800

    Updated
    added steam_guard check

--- Steam Automation/steam_automation.py
+++ Steam Automation/steam_automation.py
@@ -24,7 +24,49 @@ try:
     pyautogui.typewrite(credentials[2])
     print('Entering password...'+str(len(credentials[2]))+" length")
     pyautogui.typewrite(['enter'])
-    print('Done!')
+    time.sleep(2)
+
+    #if steam guard
+    sguard_found = pyautogui.locateOnScreen('sguard.png')
+    if sguard_found is not None:
+        print('This account seems to be running on an authorization.')
+        time.sleep(0.5)
+        print('Searching for any available backup codes in PC..')
+        time.sleep(0.5)
+        try:
+            x, y = 1068, 588
+            code_path = 'C:\\Users\\admin\\Desktop\\codes.txt'
+
+            with open(code_path, 'r') as f:
+                print(code_path.split("\\")[-1] + " containing backup codes was found!")
+                codelist = []
+                for line in f:
+                    codes = line.split('\n')
+                    codelist.append(codes[0])
+                print("File is holding " + str(len(codelist)) + " backup codes.")
+                #Getting first code in the list
+                getCode = codelist[0]
+                pyautogui.click(x, y, clicks=1, button='left')
+                pyautogui.typewrite(getCode)
+                pyautogui.typewrite(['enter'])
+                #Removing used code
+                codelist.pop(0)
+                #Updating codelist
+                codes = ""
+                for code in codelist:
+                    codes += code + "\n"
+                with open('C:\\Users\\admin\\Desktop\\codes.txt', 'w') as d:
+                    d.write(codes)
+                time.sleep(0.5)
+                print('Done..! Logging-in.')
+
+        except FileNotFoundError:
+            print('Could not find any files with steam guard codes.')
+
+    else:
+        print('Account does not require any subsequent authentication.')
+        time.sleep(0.5)
+        print('Done..! Logging-in.')
 
 except FileNotFoundError:
     credentials = ["","",""]
